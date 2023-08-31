70b89413e06d9cb347006e28c938e63073d8e108
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-31 18:29:27 -0300

    Add files via upload

---
+++ EX3POOMC/Quadrado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3POOMC
+{
+    internal class Quadrado
+    {
+        private double diagonal;
+
+
+
+        public Quadrado(double diagonal)
+
+        {
+
+            this.diagonal = diagonal;
+
+        }
+
+
+
+        public double CalcularArea()
+
+        {
+
+            Double lado = diagonal / Math.Sqrt(2);
+
+            Double area = lado * lado;
+
+            return area;
+
+        }
+    }
+}
